d977fd1b8f387693e5baf88f64b50de939164cc5
jyothika28 <jyothikajalla032@gmail.com>
2025-03-21 21:45:07 -0400

    demo section

--- client/src/components/CodeSync.js
+++ client/src/components/CodeSync.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Hero from './hero/Hero';
+import DemoSection from './demosection/Demosection';
 function CodeSync() {
     return (
         <>
         <Hero />
+        <DemoSection />
         </>
     )
 }
---
+++ client/src/components/demosection/Demosection.js
@@ -0,0 +1,8 @@
+function DemoSection() {
+  return (
+    <div>
+      <h1>Demo Section</h1>
+    </div>
+  );
+}   
+export default DemoSection;
